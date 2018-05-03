3397b40927e83407e256791d815a0f84d1f664db
Unknown <richard2013rf@gmail.Com>
2018-05-03 09:51:12 +0200

    Define Location et ses dépendance
    Afin de pouvoir faire un $location et naviguer en interne, toujours pas fonctionnel

--- src/pages/sandwich/sandwich.ts
+++ src/pages/sandwich/sandwich.ts
@@ -1,17 +1,18 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams, AlertController } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, AlertController} from 'ionic-angular';
+import { Location } from 'angular/common';
 import { TaillePage } from '../taille/taille';
 import { PanierPage } from '../panier/panier';
-
-
-
+ 
+ 
+ 
 /**
  * Generated class for the SandwichPage page.
  *
  * See https://ionicframework.com/docs/components/#navigation for more info on
  * Ionic pages and navigation.
  */
-
+ 
  
 @IonicPage()
 @Component({
@@ -19,11 +20,11 @@ import { PanierPage } from '../panier/panier';
   templateUrl: 'sandwich.html',
 })
 export class SandwichPage {
-
-	taille = TaillePage
-	panier = PanierPage
-	
-  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController) {
+ 
+  taille = TaillePage
+  panier = PanierPage
+  
+  constructor(public navCtrl: NavController, public navParams: NavParams, public alertCtrl: AlertController, private authService:AuthService, private router:Router, private location:Location ) {
   }
   
 presentConfirm() {
@@ -39,24 +40,23 @@ presentConfirm() {
         }
       },
       {
-        text: 'Oui',
+		  text: 'Oui',
         handler: () => {
         console.log('Yes clicked'),
-		window.location.href='http://www.youtube.com'; /*ici il faut reussir a trouver un lien pour aller vers panierPage*/
-		 
+    $location.path("../pages/panier/panier");
+     
         }
       }
     ]
   });
   alert.present();
 }
-
+ 
   ionViewDidLoad() {
     console.log('ionViewDidLoad SandwichPage');
   }
   
   
-
+ 
 }
-
-
+ 
